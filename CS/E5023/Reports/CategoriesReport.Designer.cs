87bedbdcdcf819768e0e5711e40a594b957f2640
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/E5023/Reports/CategoriesReport.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace E5023.Reports {
+    partial class CategoriesReport {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+            this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
+            this.xrLabel2 = new DevExpress.XtraReports.UI.XRLabel();
+            this.pbImage = new DevExpress.XtraReports.UI.XRPictureBox();
+            this.ReportHeader = new DevExpress.XtraReports.UI.ReportHeaderBand();
+            this.lbHeader = new DevExpress.XtraReports.UI.XRLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // Detail
+            // 
+            this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.pbImage,
+            this.xrLabel2,
+            this.xrLabel1});
+            this.Detail.Name = "Detail";
+            this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // TopMargin
+            // 
+            this.TopMargin.Name = "TopMargin";
+            this.TopMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // BottomMargin
+            // 
+            this.BottomMargin.Name = "BottomMargin";
+            this.BottomMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+            this.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(E5023.Models.Category);
+            // 
+            // xrLabel1
+            // 
+            this.xrLabel1.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "CategoryName")});
+            this.xrLabel1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Bold);
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(150.0001F, 10.00001F);
+            this.xrLabel1.Name = "xrLabel1";
+            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
+            this.xrLabel1.SizeF = new System.Drawing.SizeF(499.9999F, 33.70832F);
+            this.xrLabel1.StylePriority.UseFont = false;
+            this.xrLabel1.Text = "lbCategoryName";
+            // 
+            // xrLabel2
+            // 
+            this.xrLabel2.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Text", null, "Description")});
+            this.xrLabel2.Font = new System.Drawing.Font("Times New Roman", 14F);
+            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(150.0001F, 43.70832F);
+            this.xrLabel2.Name = "xrLabel2";
+            this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
+            this.xrLabel2.SizeF = new System.Drawing.SizeF(499.9999F, 46.29168F);
+            this.xrLabel2.StylePriority.UseFont = false;
+            this.xrLabel2.Text = "lbDescription";
+            // 
+            // pbImage
+            // 
+            this.pbImage.DataBindings.AddRange(new DevExpress.XtraReports.UI.XRBinding[] {
+            new DevExpress.XtraReports.UI.XRBinding("Image", null, "Picture")});
+            this.pbImage.LocationFloat = new DevExpress.Utils.PointFloat(0F, 10.00001F);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.SizeF = new System.Drawing.SizeF(150.0001F, 79.99998F);
+            this.pbImage.Sizing = DevExpress.XtraPrinting.ImageSizeMode.ZoomImage;
+            // 
+            // ReportHeader
+            // 
+            this.ReportHeader.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.lbHeader});
+            this.ReportHeader.Name = "ReportHeader";
+            // 
+            // lbHeader
+            // 
+            this.lbHeader.Font = new System.Drawing.Font("Times New Roman", 36F);
+            this.lbHeader.LocationFloat = new DevExpress.Utils.PointFloat(0F, 10.00001F);
+            this.lbHeader.Name = "lbHeader";
+            this.lbHeader.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
+            this.lbHeader.SizeF = new System.Drawing.SizeF(650F, 79.99998F);
+            this.lbHeader.StylePriority.UseFont = false;
+            this.lbHeader.StylePriority.UseTextAlignment = false;
+            this.lbHeader.Text = "Categories Report";
+            this.lbHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            // 
+            // CategoriesReport
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail,
+            this.TopMargin,
+            this.BottomMargin,
+            this.ReportHeader});
+            this.DataSource = this.bindingSource1;
+            this.Version = "13.2";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraReports.UI.DetailBand Detail;
+        private DevExpress.XtraReports.UI.TopMarginBand TopMargin;
+        private DevExpress.XtraReports.UI.BottomMarginBand BottomMargin;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private DevExpress.XtraReports.UI.XRPictureBox pbImage;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel2;
+        private DevExpress.XtraReports.UI.XRLabel xrLabel1;
+        private DevExpress.XtraReports.UI.ReportHeaderBand ReportHeader;
+        private DevExpress.XtraReports.UI.XRLabel lbHeader;
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to bind a report displayed in the DocumentViewer extension to the model's data
+
+
+<p>This example demonstrates how you can bind a Report in your ASP.NET MVC application to your model class.</p>
+<p><strong> </strong></p>
+<p>Starting with version <strong>14.2 (2014 vol 2)</strong>, use the ObjectDataSource component to bind your report to your Model class schema as demonstrated in the <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument17784">How to: Bind a Report to an Object Data Source</a> help topic.<br /><br /></p>
+<p>In <strong>earlier</strong> versions, to populate the XtraReport Field List window with your model properties, add the <a href="http://msdn.microsoft.com/en-us/library/system.windows.forms.bindingsource%28v=vs.100%29.aspx">BindingSource</a> component to your report, as shown in the <a href="http://documentation.devexpress.com/#XtraReports/CustomDocument5194">Providing Data to a Web Report</a> help topic.</p>
+
+<br/>
+
+
